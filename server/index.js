fe90c46d6ff773a375e4ccea21376106067b2725
mgniedko <max.gnedko@gmail.com>
2023-05-03 21:16:27 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from "./mongodb/connect.js";
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({limit: '50gb'}));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
+app.get('/', async (req, res) => {
+   res.send('Hello from DALL-E!');
+});
+
+const startServer = async () => {
+    try {
+        connectDB(process.env.MONGODB_URL)
+    } catch (err) {
+        console.log(err);
+    }
+
+    app.listen(8080, () => console.log('Server started on 8080'));
+}
+
+startServer();
